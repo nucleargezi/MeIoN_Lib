c7331a3099e3698ccdac918d461a6c4f12044adb
nucleargezi <604223110@qq.com>
2024-10-29 20:34:40 +0800

    master

--- MeIoN_debug.hpp
+++ MeIoN_debug.hpp
@@ -1,28 +1,3 @@
-#pragma once
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <cctype>
-#include <chrono>
-#include <cmath>
-#include <cstring>
-#include <ctime>
-#include <fstream>
-#include <functional>
-#include <iomanip>
-#include <iostream>
-#include <limits>
-#include <map>
-#include <queue>
-#include <random>
-#include <ranges>
-#include <set>
-#include <stack>
-#include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
 template <class T, size_t size = std::tuple_size<T>::value>
 std::string to_debug(T, std::string s = "")
     requires(not std::ranges::range<T>);
