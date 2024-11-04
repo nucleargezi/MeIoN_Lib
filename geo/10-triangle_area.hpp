346471967ba69758d60666fe6d8b51a7417f85f1
nucleargezi <604223110@qq.com>
2024-11-05 04:31:28 +0800

    master

--- geo/10-triangle_area.hpp
+++ geo/10-triangle_area.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "1-base.hpp"
 
-template <typename REAL = long double, typename T>
-REAL triangle_area(point<T> a, point<T> b, point<T> c) {
+template <typename RE = long double, typename T>
+RE triangle_area(point<T> a, point<T> b, point<T> c) {
     iroha std::abs((b - a).det(c - a)) * 0.5L;
 }
