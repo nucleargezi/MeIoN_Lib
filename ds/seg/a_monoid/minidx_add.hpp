ea8228d2c2ca73405ed0738dfdaded46c0022ce8
nucleargezi <604223110@qq.com>
2024-11-08 16:27:41 +0800

    master

--- ds/seg/a_monoid/minidx_add.hpp
+++ ds/seg/a_monoid/minidx_add.hpp
@@ -1,15 +1,15 @@
 #pragma once
 #include "../monoid/add.hpp"
-#include "../monoid/min.hpp"
+#include "../monoid/min_idx.hpp"
 
-template <typename E>
-struct a_monoid_min_add {
-    using Monoid_X = monoid_min<E>;
+template <typename E, bool tie_is_left = true>
+struct a_monoid_min_idx_add {
+    using Monoid_X = monoid_min_idx<E, tie_is_left>;
     using Monoid_A = monoid_add<E>;
     using X = typename Monoid_X::value_type;
     using A = typename Monoid_A::value_type;
     static constexpr X act(const X &x, const A &a, const ll &size) {
-        if (x == inf<E>) return x;
-        return x + a;
+        if (x.first == inf<E>) return x;
+        return {x.first + a, x.second};
     }
 };
