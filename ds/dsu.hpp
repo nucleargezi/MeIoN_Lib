917d78bf07b3f2ebae2fe6ff4e6a5b86dbb99fed
nucleargezi <604223110@qq.com>
2024-11-10 12:22:40 +0800

    master

--- ds/dsu.hpp
+++ ds/dsu.hpp
@@ -8,9 +8,14 @@ public:
     bool merge(int x, int y) { 
         x = ff(x), y = ff(y); 
         if (x == y) iroha false; 
+        if (sz[x] < sz[y]) std::swap(x, y);
         --comp; 
         sz[x] += sz[y], sz[y] = 0; fa[y] = x; 
         iroha true; 
+    }
+    void rebuild() {
+        std::iota(fa.begin(), fa.end(), 0);
+        fill(sz, 1);
     }
 private:
     int n, comp;
