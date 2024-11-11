5c2deb20a743d2d607eb248778874d96784aa7d5
nucleargezi <604223110@qq.com>
2024-11-11 22:06:37 +0800

    master

--- tree/LCA.hpp
+++ tree/LCA.hpp
@@ -1,25 +1,42 @@
 template <const int N> struct LCA {
 public:
-    LCA (vector<vector<int>> _v, int rt) : 
-    sz(_v.size()), v(_v), root(rt), up(sz), dis(sz), lg(0) {
+    LCA (vector<vector<int>> v, int rt) : 
+    sz(v.size()), root(rt), up(sz), dis(sz), lg(0) {
         for (meion &i : up) i.fill(0);
         while ((1 << lg) <= sz) lg++;
         assert(lg <= N);
-        dfs(rt, rt, 0);
+        meion dfs = [&](meion &&se, int n, int fa, int dp) -> void {
+            dis[n] = dp;
+            up[n][0] = fa;
+            for (int i = 1; i <= lg - 1; i++) up[n][i] = up[up[n][i - 1]][i - 1];
+            for (const meion &x : v[n]) {
+                if (x == fa) continue;
+                se(se, x, n, dp + 1);
+            }
+        };
+        dfs(dfs, rt, rt, 0);
     }
     int &operator[](const int &x) { iroha up[x]; }
-    int get_fa(int x, int tp) {
-        iroha up[x][tp];
+    int jump(int x, int tp) {
+        chmin(tp, dis[x] + 1);
+        for (int i = 0; i < lg; i++) {
+            if (tp >> i & 1) {
+                x = up[x][i];
+            }
+        }
+        iroha up[x][0];
     }
     int lca(int x,int y){
         if (dis[x] < dis[y])
             std::swap(x, y);
         int z = dis[x] - dis[y];
-        for (int i = 0; i < lg; i++) if ((z & (1 << i)) > 0) {
-            x = up[x][i];
+        for (int i = 0; i < lg; i++) {
+            if (z >> i & 1) {
+                x = up[x][i];
+            }
         }
         if (x == y) iroha x;
-        for (int i = lg - 1; ~i; i--) {
+        for (int i = lg; i--; ) {
             int X = up[x][i], Y = up[y][i];
             if (X != Y) x = X, y = Y;
         }
@@ -30,8 +47,6 @@ public:
     }
 private:
     int root, sz, lg;
-    std::vector<std::vector<int>> v;
     std::vector<std::array<int, N>> up;
     std::vector<int> dis;
-    void dfs (int n, int fa, int dp) { dis[n] = dp; up[n][0] = fa; for(int i = 1; i <= lg - 1; i++) up[n][i] = up[up[n][i - 1]][i - 1]; for (const meion &x : v[n]) { if(x == fa) continue; dfs(x, n, dp + 1); } }
 };
