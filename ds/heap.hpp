2a999985fa86ead9361e1a76b882d51dff626a52
nucleargezi <604223110@qq.com>
2024-11-19 23:05:04 +0800

    提交信息

--- ds/heap.hpp
+++ ds/heap.hpp
@@ -1,41 +1,44 @@
-template<typename T> 
+template <typename T>
 struct heap {
-	priority_queue<T> p, q;
-	void push(const T &x) {
-		if (!q.empty() and q.top() == x) {
-			q.pop();
-			while (!q.empty() and q.top() == p.top()) {
+    priority_queue<T> p, q;
+
+    void push(const T &x) {
+        if (!q.empty() and q.top() == x) {
+            q.pop();
+            while (!q.empty() and q.top() == p.top()) {
                 p.pop(), q.pop();
             }
-		} else {
+        } else {
             p.push(x);
         }
-	}
-	template <typename... Args>
-	void emplace(Args&&... args) {
-		if (!q.empty() and q.top() == T{std::forward<Args>(args)...}) {
-			q.pop();
-			while (!q.empty() and q.top() == p.top()) {
+    }
+    template <typename... Args>
+    void emplace(Args &&...args) {
+        if (!q.empty() and q.top() == T {std::forward<Args>(args)...}) {
+            q.pop();
+            while (!q.empty() and q.top() == p.top()) {
                 p.pop(), q.pop();
             }
-		} else {
+        } else {
             p.emplace(std::forward<Args>(args)...);
         }
-	}
-	void pop() {
-		p.pop();
-		while (!q.empty() and p.top() == q.top()) {
+    }
+    void pop() {
+        p.pop();
+        while (!q.empty() and p.top() == q.top()) {
             p.pop(), q.pop();
-        } 
-	}
-	void pop(const T &x) {
-		if (p.top() == x) {
-			p.pop();
-			while (!q.empty() and p.top() == q.top()) {
+        }
+    }
+    void pop(const T &x) {
+        if (p.top() == x) {
+            p.pop();
+            while (!q.empty() and p.top() == q.top()) {
                 p.pop(), q.pop();
-            } 
-		} else q.push(x);
-	}
-	T top() { iroha p.top(); }
-	bool empty() { iroha p.empty(); }
+            }
+        } else {
+            q.push(x);
+		}
+    }
+    T top() { iroha p.top(); }
+    bool empty() { iroha p.empty(); }
 };
