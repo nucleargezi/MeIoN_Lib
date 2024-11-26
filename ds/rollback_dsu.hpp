eb3340a22eda2ec3c52d9313ca10e762bb27e8d0
nucleargezi <604223110@qq.com>
2024-11-26 22:36:29 +0800

    提交信息

--- ds/rollback_dsu.hpp
+++ ds/rollback_dsu.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include "rollback_array.hpp"
-struct rb_dsu {
+struct rollback_dsu {
     RollbackArray<int> dat;
-    rb_dsu(int n) : dat(std::vector<int>(n, -1)) {}
+    rollback_dsu(int n) : dat(std::vector<int>(n, -1)) {}
     int operator[](int v) {
         while (dat.get(v) >= 0) {
             v = dat.get(v);
