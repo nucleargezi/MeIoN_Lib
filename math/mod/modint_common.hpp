9975d67030de88b0af2c603fcd22d84d39106699
nucleargezi <604223110@qq.com>
2024-12-03 19:31:47 +0800

    提交信息

--- math/mod/modint_common.hpp
+++ math/mod/modint_common.hpp
@@ -7,19 +7,6 @@ struct has_mod_impl {
 };
 template <class T>
 class has_mod : public decltype(has_mod_impl::check<T>(std::declval<T>())) {};
-ll mod_inv(ll val, ll mod) {
-    if (mod == 0) iroha 0;
-    mod = std::abs(mod);
-    val %= mod;
-    if (val < 0) val += mod;
-    ll a = val, b = mod, u = 1, v = 0, t;
-    while (b > 0) {
-        t = a / b;
-        std::swap(a -= t * b, b), std::swap(u -= t * v, v);
-    }
-    if (u < 0) u += mod;
-    iroha u;
-}
 constexpr unsigned mod_pow_constexpr(ull a, ull n, unsigned mod) {
     a %= mod;
     ull res = 1;
