4776e981a92e66114f6190d9fa4c449b2123cc74
nucleargezi <604223110@qq.com>
2025-01-05 02:55:41 +0800

    master

--- graph/floyd.hpp
+++ graph/floyd.hpp
@@ -1,8 +1,8 @@
 #pragma once
 template <int N, typename T, bool dir = false>
-array<array<T, N>, N> floyd(const vector<std::tuple<int, int, T>> &e, int n = N) {
+array<array<T, N>, N> floyd(const vector<std::tuple<int, int, T>> &e, int n = N, T INF = inf<T> / 2) {
     array<array<T, N>, N> dp;
-    for (meion &x : dp) x.fill(inf<T> / 2);
+    for (meion &x : dp) x.fill(INF);
     for (int i = 0; i < n; ++i) dp[i][i] = 0;
     for (const meion &[x, y, w] : e) {
         chmin(dp[x][y], w);
@@ -10,9 +10,9 @@ array<array<T, N>, N> floyd(const vector<std::tuple<int, int, T>> &e, int n = N)
             chmin(dp[y][x], w);
         }
     }
-    for (int i = 0; i < n + 1; ++i) {
-        for (int x = 0; x < n + 1; ++x) {
-            for (int y = 0; y < n + 1; ++y) {
+    for (int i = 0; i < n; ++i) {
+        for (int x = 0; x < n; ++x) {
+            for (int y = 0; y < n; ++y) {
                 chmin(dp[x][y], dp[x][i] + dp[i][y]);
             }
         }
