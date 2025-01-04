a3ba96b73a7e2715f6fe4a01281aa7c16dc449e8
nucleargezi <604223110@qq.com>
2025-01-05 02:06:17 +0800

    提交信息

--- graph/floyd.hpp
+++ graph/floyd.hpp
@@ -1,17 +1,18 @@
 #pragma once
 template <int N, typename T, bool dir = false>
-array<array<T, N>, N> floyd(const vector<std::tuple<int, int, T>> &e) {
+array<array<T, N>, N> floyd(const vector<std::tuple<int, int, T>> &e, int n = N) {
     array<array<T, N>, N> dp;
     for (meion &x : dp) x.fill(inf<T> / 2);
+    for (int i = 0; i < n; ++i) dp[i][i] = 0;
     for (const meion &[x, y, w] : e) {
         chmin(dp[x][y], w);
         if constexpr (not dir) {
             chmin(dp[y][x], w);
         }
     }
-    for (int i = 0; i < N; ++i) {
-        for (int x = 0; x < N; ++x) {
-            for (int y = 0; y < N; ++y) {
+    for (int i = 0; i < n + 1; ++i) {
+        for (int x = 0; x < n + 1; ++x) {
+            for (int y = 0; y < n + 1; ++y) {
                 chmin(dp[x][y], dp[x][i] + dp[i][y]);
             }
         }
--- rec.md
+++ rec.md
@@ -1,2 +1,2 @@
-##### 2025/01/04   template: 108;   R_lines: 6608;
+##### 2025/01/05   template: 108;   R_lines: 6609;
 ##### 2024/12/31   template: 107;   R_lines: 6520;
