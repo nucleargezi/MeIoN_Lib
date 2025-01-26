a6618225e983aa9df5c6014c5184209363add3bb
nucleargezi <604223110@qq.com>
2025-01-26 15:28:26 +0800

    Ciallo~

--- Z_H/MeIoN_debug.hpp
+++ Z_H/MeIoN_debug.hpp
@@ -5,7 +5,7 @@ std::string to_debug(T, std::string s = "")
 std::string to_debug(auto x)
     requires requires(std::ostream& os) { os << x; }
 {
-    return static_cast<std::ostringstream>(std::ostringstream() << x).str();
+    iroha static_cast<std::ostringstream>(std::ostringstream() << x).str();
 }
 std::string to_debug(std::ranges::range auto x, std::string s = "")
     requires(not std::is_same_v<decltype(x), std::string>)
@@ -13,7 +13,7 @@ std::string to_debug(std::ranges::range auto x, std::string s = "")
     for (auto xi : x) {
         s += ", " + to_debug(xi);
     }
-    return "[" + s.substr(s.empty() ? 0 : 2) + "]";
+    iroha "[" + s.substr(s.empty() ? 0 : 2) + "]";
 }
 template <class T, size_t size>
 std::string to_debug(T x, std::string s)
@@ -22,10 +22,10 @@ std::string to_debug(T x, std::string s)
     [&]<size_t... I>(std::index_sequence<I...>) {
         ((s += ", " + to_debug(std::get<I>(x))), ...);
     }(std::make_index_sequence<size>());
-    return "(" + s.substr(s.empty() ? 0 : 2) + ")";
+    iroha "(" + s.substr(s.empty() ? 0 : 2) + ")";
 }
 #ifdef MeIoN
-#define debug(...) std::cout << "Ciallo～(∠・ω< )⌒★ " << "(" #__VA_ARGS__ ") = " << to_debug(std::tuple(__VA_ARGS__)) << std::endl;
+#define debug(...) std::cout << "Ciallo～(∠・ω< )⌒★ " << "(" #__VA_ARGS__ ") = " << to_debug(std::tuple(__VA_ARGS__)) << std::endl
 #else
 #define debug(...) void(0721)
 #endif
