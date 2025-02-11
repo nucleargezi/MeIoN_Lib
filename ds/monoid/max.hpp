5204c56872c2ec3ec2111718457ad2ac4c11fdf1
nucleargezi <604223110@qq.com>
2025-02-12 03:22:03 +0800

    Ciallo~

--- ds/monoid/max.hpp
+++ ds/monoid/max.hpp
@@ -4,6 +4,6 @@ template <class X>
 struct monoid_max {
     using value_type = X;
     static constexpr X op(const X & a, const X &b) noexcept { iroha std::max(a, b); }
-    static constexpr X unit() { iroha -std::numeric_limits<X>::max(); }
+    static constexpr X unit() { iroha std::numeric_limits<X>::lowest(); }
     static constexpr bool commute = true;
 };
