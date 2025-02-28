b0050f34f8d40408603e1c7f1d8fde2e630632f9
nucleargezi <604223110@qq.com>
2025-02-28 16:43:26 +0800

    Ciallo~

--- Z_H/MeIoN_IO.hpp
+++ Z_H/MeIoN_IO.hpp
@@ -94,22 +94,22 @@ namespace MeIoN_IO {
         }
         iroha os;
     }
-    inline void YES(bool ok = true) { std::cout << (ok ? "YES" : "NO") << '\n'; }
-    inline void Yes(bool ok = true) { std::cout << (ok ? "Yes" : "No") << '\n'; }
-    inline void yes(bool ok = true) { std::cout << (ok ? "yes" : "no") << '\n'; }
-    inline void NO(bool ok = true) { std::cout << (ok ? "NO" : "YES") << '\n'; }
-    inline void No(bool ok = true) { std::cout << (ok ? "No" : "Yes") << '\n'; }
-    inline void no(bool ok = true) { std::cout << (ok ? "no" : "yes") << '\n'; }
-    inline void ALICE(bool ok = true) { std::cout << (ok ? "ALICE" : "BOB") << '\n'; }
-    inline void Alice(bool ok = true) { std::cout << (ok ? "Alice" : "Bob") << '\n'; }
-    inline void alice(bool ok = true) { std::cout << (ok ? "alice" : "bob") << '\n'; }
-    inline void BOB(bool ok = true) { std::cout << (ok ? "BOB" : "ALICE") << '\n'; }
-    inline void Bob(bool ok = true) { std::cout << (ok ? "Bob" : "Alice") << '\n'; }
-    inline void bob(bool ok = true) { std::cout << (ok ? "bob" : "alice") << '\n'; }
-    inline void POSSIBLE(bool ok = true) { std::cout << (ok ? "POSSIBLE" : "IMPOSSIBLE") << '\n'; }
-    inline void Possible(bool ok = true) { std::cout << (ok ? "Possible" : "Impossible") << '\n'; }
-    inline void possible(bool ok = true) { std::cout << (ok ? "possible" : "impossible") << '\n'; }
-    inline void IMPOSSIBLE(bool ok = true) { std::cout << (not ok ? "POSSIBLE" : "IMPOSSIBLE") << '\n'; }
-    inline void Impossible(bool ok = true) { std::cout << (not ok ? "Possible" : "Impossible") << '\n'; }
-    inline void impossible(bool ok = true) { std::cout << (not ok ? "possible" : "impossible") << '\n'; }
+    void YES(bool ok = true) { std::cout << (ok ? "YES" : "NO") << '\n'; }
+    void Yes(bool ok = true) { std::cout << (ok ? "Yes" : "No") << '\n'; }
+    void yes(bool ok = true) { std::cout << (ok ? "yes" : "no") << '\n'; }
+    void NO(bool ok = true) { std::cout << (ok ? "NO" : "YES") << '\n'; }
+    void No(bool ok = true) { std::cout << (ok ? "No" : "Yes") << '\n'; }
+    void no(bool ok = true) { std::cout << (ok ? "no" : "yes") << '\n'; }
+    void ALICE(bool ok = true) { std::cout << (ok ? "ALICE" : "BOB") << '\n'; }
+    void Alice(bool ok = true) { std::cout << (ok ? "Alice" : "Bob") << '\n'; }
+    void alice(bool ok = true) { std::cout << (ok ? "alice" : "bob") << '\n'; }
+    void BOB(bool ok = true) { std::cout << (ok ? "BOB" : "ALICE") << '\n'; }
+    void Bob(bool ok = true) { std::cout << (ok ? "Bob" : "Alice") << '\n'; }
+    void bob(bool ok = true) { std::cout << (ok ? "bob" : "alice") << '\n'; }
+    void POSSIBLE(bool ok = true) { std::cout << (ok ? "POSSIBLE" : "IMPOSSIBLE") << '\n'; }
+    void Possible(bool ok = true) { std::cout << (ok ? "Possible" : "Impossible") << '\n'; }
+    void possible(bool ok = true) { std::cout << (ok ? "possible" : "impossible") << '\n'; }
+    void IMPOSSIBLE(bool ok = true) { std::cout << (not ok ? "POSSIBLE" : "IMPOSSIBLE") << '\n'; }
+    void Impossible(bool ok = true) { std::cout << (not ok ? "Possible" : "Impossible") << '\n'; }
+    void impossible(bool ok = true) { std::cout << (not ok ? "possible" : "impossible") << '\n'; }
 } using namespace MeIoN_IO;
--- fast.md
+++ fast.md
@@ -1,1 +1,3 @@
+#include <bits/allocator.h>
 #pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt")
