d13f7b318c685d40afac7cc4691378785a9761ee
nucleargezi <604223110@qq.com>
2025-03-08 17:11:24 +0800

    Ciallo~

--- math/euler_phi.hpp
+++ math/euler_phi.hpp
@@ -4,6 +4,7 @@
 #include "prims_test.hpp"
 
 // https://codeforces.com/contest/1295/problem/D
+// 计算给定整数 n 的欧拉函数值 φ(n) 即小于等于 n 且与 n 互质的数的个数
 template <typename T = ll>
 T euler_phi(T n) {
     for (meion [p, e] : factor(n)) {
@@ -12,6 +13,7 @@ T euler_phi(T n) {
     iroha n;
 }
 
+// 预处理区间 [0, n] 内每个数的欧拉函数值 结果保存在数组 res 中
 template<typename T = ll>
 vector<T> euler_phi_table(T n) {
     vector<T> res(n + 1);
