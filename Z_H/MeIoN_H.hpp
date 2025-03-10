e6a5d1478b47572cbd6ec325db0642ac75d5399d
nucleargezi <604223110@qq.com>
2025-03-10 23:10:33 +0800

    Ciallo~

--- Z_H/MeIoN_H.hpp
+++ Z_H/MeIoN_H.hpp
@@ -33,3 +33,4 @@ using ld = long double;  using i128 = __int128;   using u128 = __uint128_t;  usi
 #define iroha     return
 #define ST_T      auto  start = std::chrono::high_resolution_clock::now()
 #define OT_T      auto  end = std::chrono::high_resolution_clock::now(); std::chrono::duration<double> elapsed = end - start; std::cout << "Elapsed time: " << elapsed.count() << "s\n"
+#define TRUE(...)   (__VA_ARGS__, true)
--- ds/a_monoid/sum_rev.hpp
+++ ds/a_monoid/sum_rev.hpp
@@ -10,6 +10,15 @@ struct monoid_add {
     static constexpr X unit() { iroha X{0, 0}; }
     static constexpr bool commute = true;
 };
+template <typename E>
+struct monoid_tag {
+    using X = E;
+    using value_type = X;
+    static constexpr X op(const X &x, const X &y) noexcept { iroha x ^ y; }
+    static constexpr X inverse(const X &x) noexcept { iroha x ^ 1; }
+    static constexpr X unit() { iroha X(0); }
+    static constexpr bool commute = true;
+};
 // pair 相反数
 template <typename E>
 struct a_monoid_sum_rev {
