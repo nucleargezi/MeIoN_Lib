e10579fff755742a4907ff0d9cf6ec4418772947
nucleargezi <604223110@qq.com>
2025-04-05 03:33:05 +0800

    Ciallo~

--- math/exgcd.hpp
+++ math/exgcd.hpp
@@ -7,3 +7,10 @@ ll exgcd(ll a, ll b, ll &x, ll &y){
     y -= a / b * x;
     iroha d;
 }
+// {x, y} : ax + by == c
+tuple<bool, ll, ll> linear_solver(ll a, ll b, ll c) {
+    ll x, y;
+    ll gcd{exgcd(a, b, x, y)};
+    if (c % gcd) iroha {0, -1, -1};
+    iroha {1, c / gcd * x, c / gcd * y};
+}
