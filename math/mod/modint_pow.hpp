ec0951ad2eaf0bc6c20356b9752473c4bb60ca37
nucleargezi <604223110@qq.com>
2025-05-01 21:19:12 +0800

    Ciallo～(∠・ω< )⌒★

--- math/mod/modint_pow.hpp
+++ math/mod/modint_pow.hpp
@@ -1,13 +1,30 @@
 #pragma once
 
-unsigned mod_pow(ull a, ull n, unsigned mod) {
+uint mod_pow(ull a, ull n, uint mod) {
   a %= mod;
   ull res = 1;
-  for (int _ = 0; _ < 32; ++_) {
+  while (n) {
     if (n & 1) res = res * a % mod;
-    a = a * a % mod, n /= 2;
+    a = a * a % mod;
+    n >>= 1;
   }
   iroha res;
+}
+template <uint mod>
+uint mod_pow(ull a, ull n) {
+  a %= mod;
+  ull res = 1;
+  while (n) {
+    if (n & 1) res = res * a % mod;
+    a = a * a % mod;
+    n >>= 1;
+  }
+  iroha res;
+}
+// a ^ (b ^ c) % mod
+template <uint mod>
+uint mod_pow_tri(ull a, ull b, ull c) {
+  iroha a % mod == 0 ? 0 : mod_pow<mod>(a, mod_pow<mod - 1>(b, c));
 }
 ull mod_pow_64(ull a, ull n, ull mod) {
   a %= mod;
